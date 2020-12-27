cdef900cff33326fba24871834ce206a7eff5743
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2020-12-27 20:29:14 +0530

    Added some betterments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 const { spawn } = require("child_process");
-
+const fs = require("fs");
 function runCommand(command, args, options = undefined) {
   const spawned = spawn(command, args, options);
 
@@ -32,9 +32,34 @@ const main = async (repositoryUrl) => {
   await runCommand("git", ["clone", repositoryUrl, directoryName]);
   // Removing the .git folder
   await runCommand("rm", ["-rf", `${directoryName}/.git`]);
-  console.log("Now, installing the dependencies...");
+  //1. Removing the readme, package-lock.json
+  await runCommand("rm", ["-r", `${directoryName}/readme.md`]);
+  await runCommand("rm", ["-r", `${directoryName}/package-lock.json`]);
+  //2. rename .env.example to .env
+  await runCommand("mv", [
+    `${directoryName}/.env.example`,
+    `${directoryName}/.env`,
+  ]);
+
+  //3. get the json from package.json
+  const packageJsonRaw = fs.readFileSync(`${directoryName}/package.json`);
+  const packageJson = JSON.parse(packageJsonRaw);
+  // and modify its detail
+  packageJson.name = directoryName;
+  packageJson.author = "";
+  packageJson.keywords = [];
+  packageJson.version = "1.0.0";
+  packageJson.description = "A node application";
+  // and again write it again into package.json
+  await runCommand("rm", ["-r", `${directoryName}/package.json`]);
+  fs.writeFileSync(
+    `${directoryName}/package.json`,
+    JSON.stringify(packageJson, null, 2)
+  );
   // Installing the dependencies.
+  console.log("Now, installing the dependencies...");
   await runCommand("npm", ["i"], { cwd: `${process.cwd()}/${directoryName}` });
+
   console.log(`Application generated is ready to use.
 
 To get started, 
